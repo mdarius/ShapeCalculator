af4ed74e238e5e7674afff87231f0cbdf3d7e061
Darius Darius <2@hostlink.lt>
2011-12-23 07:55:19 +0200

    fix

--- src/java/web/controller/AbstractController.java
+++ src/java/web/controller/AbstractController.java
@@ -4,7 +4,7 @@ package web.controller;
 import facade.iGeoFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class AbstractController {
+public abstract class AbstractController {
        
    @Autowired protected  iGeoFacade geofacade;   
     
